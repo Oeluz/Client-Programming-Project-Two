6142fee56e6ead21e28b24595c6ad33daf9a215a
Zhencheng Chen <42793066+Oeluz@users.noreply.github.com>
2021-11-04 20:06:31 -0400

    Completed degree.js

--- javascript/get-about.js
+++ javascript/get-about.js
@@ -3,16 +3,38 @@ getData({
 })
 .done((data) => {
     console.log(data);
-
-    $('<div/>', {
-        text: 'About',
-        id: 'about'
-    }).appendTo('body');
+    
+    $('#about').addClass('ui-widget ui-widget-shadow section');
 
     $('<h1/>', {
-        text: data.title,
-        id: 'title'
+        text: 'ABOUT',
+        id: 'about-section-name',
+        class: 'section-name'
     }).appendTo('#about');
+    
+    $('<h2/>', {
+        text: data.title,
+        id: 'about-title'
+    }).appendTo('#about');
+
+    $('<p/>', {
+        text: data.description,
+        id: 'about-description'
+    }).appendTo('#about');
+    
+    $('<div/>', {
+        id: 'quote'
+    }).appendTo('#about');
+
+    $('<h3/>', {
+        text: "\"" + data.quote + "\"",
+        id: 'about-quote'
+    }).appendTo('#quote');
+
+    $('<h4/>', {
+        text: data.quoteAuthor,
+        id: 'about-author'
+    }).appendTo('#quote');
 }).fail(function (jqXHR) {
     console.log(jqXHR.responseText);
 });
--- javascript/get-degrees.js
+++ javascript/get-degrees.js
@@ -3,6 +3,61 @@ getData({
 })
 .done((data) => {
     console.log(data);
+
+    $('#degrees').addClass('ui-widget ui-widget-shadow section');
+    
+    $('<h1/>', {
+        text: 'DEGREES',
+        id: 'degrees-section-name',
+        class: 'section-name'
+    }).appendTo('#degrees');
+    
+    $.each(data.undergraduate, function(i, item){
+        add(item);
+    });
+
+    $.each(data.graduate, function(i, item){
+        add(item);
+    });
+
 }).fail(function (jqXHR) {
     console.log(jqXHR.responseText);
 });
+
+function add(item){
+    if(item.degreeName.indexOf("certificate") >= 0){
+        return false;
+    }
+    $('<h2/>', {
+        text: item.degreeName.toUpperCase() + " - " + item.title,
+        class: 'degrees-name'
+    }).appendTo('#degrees');
+
+    $('<p/>', {
+        text: item.description
+    }).appendTo('#degrees');
+
+    $('<div/>', {
+        id: item.degreeName
+    }).appendTo('#degrees');
+
+    $('<h3/>', {
+        text: 'Concentrations'
+    }).appendTo("#" + item.degreeName);
+
+    $('<div/>', {
+        id: item.degreeName + "-accordion"
+    }).appendTo("#" + item.degreeName);
+
+    $.each(item.concentrations, function(i, concentration) {
+        $('<h4/>', {
+            text: concentration
+        }).appendTo('#' + item.degreeName + "-accordion");
+    })
+    
+    $('#' + item.degreeName).accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: '#degrees'
+    });
+}
